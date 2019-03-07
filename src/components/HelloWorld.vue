69a1a76bb2aae19ec445db7cac1b27ab00466498
yjono <pek9dkl@gmail.com>
2019-03-08 05:47:49 +0900

    feature: データソースを飾るためのプロパティを追加する方法を2種類実装した。
    * customListはデータソースのオブジェクトにプロパティを追加する方法
    * dependencyPropertyListはデータソースの要素と同じ数だけ、データソースに依存するプロパティ要素を用意する方法
    
    customListの方がわかりやすい。
    dependencyPropertyListの方がメモリ効率がよい。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,46 @@
 <template>
-  <div></div>
+  <ul>  
+    <li 
+      v-for="(item, index) in customList"
+      :key="index"
+      class="some"
+      :class="{
+        customAttr: item.customProp, 
+        dependencyAttr: dependencyPropertyList[index]
+      }"
+    >{{item.hoge}}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      targetList: [
+        {hoge:"a", fuga:1},
+        {hoge:"b", fuga:2},
+        {hoge:"c", fuga:3},
+        {hoge:"d", fuga:3},
+        {hoge:"e", fuga:4},
+      ]
+    }
+  },
+  computed: {
+    customList() {
+      const customList = this.targetList.map(e => {
+        e.customProp = (e.fuga === 3)
+        return e;
+      });
+      return customList;
+    },
+    dependencyPropertyList() {
+      return this.targetList.map(e => {
+        const dependencyProperty = (e.fuga !== 3);
+        return dependencyProperty;
+      });
+    }
+  }
 }
 </script>
 
